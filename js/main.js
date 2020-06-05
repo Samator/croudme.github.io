841270e2c34bd05d1f452d39adabacee43a9f80d
Samator <60502930+Samator@users.noreply.github.com>
2020-06-05 19:15:06 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/*Кнопки*/
+const linkBlock1 = document.querySelector(".item_1");
+const linkBlock2 = document.querySelector(".item_2");
+const linkBlock3 = document.querySelector(".item_3");
+/*Блоки*/
+const block1 = document.querySelector("#con_sec_2_block_1");
+const block2 = document.querySelector("#con_sec_2_block_2");
+const block3 = document.querySelector("#con_sec_2_block_3");
+
+
+/*Функции*/
+linkBlock1.addEventListener('click',function(event){
+    linkBlock1.classList.add('active');
+    linkBlock2.classList.remove('active');
+    linkBlock3.classList.remove('active');
+    block1.classList.add('active_block');
+    block2.classList.remove('active_block');
+    block3.classList.remove('active_block');
+});
+linkBlock2.addEventListener('click',function(event){
+    linkBlock1.classList.remove('active');
+    linkBlock2.classList.add('active');
+    linkBlock3.classList.remove('active');
+    block1.classList.remove('active_block');
+    block2.classList.add('active_block');
+    block3.classList.remove('active_block');
+});
+linkBlock3.addEventListener('click',function(event){
+    linkBlock1.classList.remove('active');
+    linkBlock2.classList.remove('active');
+    linkBlock3.classList.add('active');
+    block1.classList.remove('active_block');
+    block2.classList.remove('active_block');
+    block3.classList.add('active_block');
+});
